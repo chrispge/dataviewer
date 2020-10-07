a3147029c508374496818453557d15d6da00f220
Chris Page <cjpage@me.com>
2020-10-07 13:52:33 +0100

    separate renderChart function

--- src/chart.js
+++ src/chart.js
@@ -1,8 +1,7 @@
 // https://dev.to/alexandrudanpop/correctly-handling-async-await-in-react-components-4h74
 // https://developer.mozilla.org/en-US/docs/Web/API/Streams_API/Using_readable_streams
 // https://www.pluralsight.com/guides/hierarchy-of-components-and-how-to-get-async-data
-import React, { Component, useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
 import {
   VictoryLine,
   VictoryChart,
@@ -17,11 +16,15 @@ function DemoChart() {
 
   useEffect(() => {
     (async () => {
-      const fetchedData = await getDataSimple();
+      const fetchedData = await getData();
       setData(fetchedData);
     })();
   }, []);
 
+  return renderChart(data);
+}
+
+function renderChart(data) {
   return (
     <div>
       <h1>Line Chart</h1>
@@ -78,8 +81,8 @@ function DemoChart() {
   );
 }
 
-async function getDataSimple() {
-  console.log("in getDataSimple");
+async function getData() {
+  console.log("in getData");
   const url = "http://localhost:3001/GenByUnit?fuel=nuclear&unit=tricastin%201";
   const response = await fetch(url);
   const data = await response.json();
@@ -87,35 +90,35 @@ async function getDataSimple() {
   return data;
 }
 
-// function getData() {
-//   const url = "http://localhost:3001/GenByUnit?fuel=nuclear&unit=tricastin%201";
-//   fetch(url)
-//     .then((response) => {
-//       const reader = response.body.getReader();
-//       // create a new stream with all the data
-//       // later if you need to you will be able to dequeue old data off this stream
-//       return new ReadableStream({
-//         start(controller) {
-//           return pump();
+function streamData() {
+  const url = "http://localhost:3001/GenByUnit?fuel=nuclear&unit=tricastin%201";
+  fetch(url)
+    .then((response) => {
+      const reader = response.body.getReader();
+      // create a new stream with all the data
+      // later if you need to you will be able to dequeue old data off this stream
+      return new ReadableStream({
+        start(controller) {
+          return pump();
 
-//           function pump() {
-//             return reader.read().then(({ done, value }) => {
-//               // When no more data needs to be consumed, close the stream
-//               if (done) {
-//                 controller.close();
-//                 return;
-//               }
-//               // Enqueue the next data chunk into target stream
-//               controller.enqueue(value);
-//               return pump();
-//             });
-//           }
-//         },
-//       });
-//     })
-//     .then((stream) => new Response(stream))
-//     .then((response) => response.json())
-//     .then((data) => renderChart(data));
-// }
+          function pump() {
+            return reader.read().then(({ done, value }) => {
+              // When no more data needs to be consumed, close the stream
+              if (done) {
+                controller.close();
+                return;
+              }
+              // Enqueue the next data chunk into target stream
+              controller.enqueue(value);
+              return pump();
+            });
+          }
+        },
+      });
+    })
+    .then((stream) => new Response(stream))
+    .then((response) => response.json())
+    .then((data) => renderChart(data));
+}
 
 export default DemoChart;
