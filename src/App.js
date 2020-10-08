7a761bea7ff4171618d4c3bc55a52eda679731a5
Chris Page <cjpage@me.com>
2020-10-08 20:33:14 +0100

    add simple styling to headers

--- src/App.js
+++ src/App.js
@@ -8,50 +8,50 @@ const apiQueryName = "GenByFuel";
 function App() {
   return (
     <div>
-      <h1>French power generation</h1>
+      <h1 class="main-title">French power generation</h1>
       <div class="grid-container">
         <div class="grid-item1">
-          <h2>Nuclear</h2>
+          <h2 class="chart-title">Nuclear</h2>
           <DemoChart {...getProps("nuclear")} />
         </div>
         <div class="grid-item">
-          <h2>Gas</h2>
+          <h2 class="chart-title">Gas</h2>
           <DemoChart {...getProps("fossil_gas")} />
         </div>
         <div class="grid-item">
-          <h2>Coal</h2>
+          <h2 class="chart-title">Coal</h2>
           <DemoChart {...getProps("fossil_hard_coal")} />
         </div>
         <div class="grid-item">
-          <h2>Hydro Reservoir</h2>
+          <h2 class="chart-title">Hydro Reservoir</h2>
           <DemoChart {...getProps("hydro_water_reservoir")} />
         </div>
         <div class="grid-item">
-          <h2>Hydro Run-of-river</h2>
-          <DemoChart {...getProps("hydro_run_of_river_and_poundage")} />;
+          <h2 class="chart-title">Hydro Run-of-river</h2>
+          <DemoChart {...getProps("hydro_run_of_river_and_poundage")} />
         </div>
         <div class="grid-item">
-          <h2>Hydro Pumped Storage</h2>
+          <h2 class="chart-title">Hydro Pumped Storage</h2>
           <DemoChart {...getProps("hydro_pumped_storage")} />
         </div>
         <div class="grid-item">
-          <h2>Wind</h2>
+          <h2 class="chart-title">Wind</h2>
           <DemoChart {...getProps("wind_onshore")} />
         </div>
         <div class="grid-item">
-          <h2>Solar</h2>
+          <h2 class="chart-title">Solar</h2>
           <DemoChart {...getProps("solar")} />
         </div>
         <div class="grid-item">
-          <h2>Oil</h2>
+          <h2 class="chart-title">Oil</h2>
           <DemoChart {...getProps("fossil_oil")} />
         </div>
         <div class="grid-item">
-          <h2>Biomass</h2>
+          <h2 class="chart-title">Biomass</h2>
           <DemoChart {...getProps("biomass")} />
         </div>
         <div class="grid-item">
-          <h2>Waste</h2>
+          <h2 class="chart-title">Waste</h2>
           <DemoChart {...getProps("waste")} />
         </div>
       </div>
