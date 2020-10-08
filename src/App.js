b1b4113c757641dd7623f57df2339b22e530234d
Chris Page <cjpage@me.com>
2020-10-08 15:10:47 +0100

    back to multiple charts

--- src/App.js
+++ src/App.js
@@ -2,40 +2,41 @@ import React from "react";
 import DemoChart from "./chart";
 
 function App() {
-  const powerChartParams = { x: "start_time", y: "mw_value" };
-  const nuclearProps = {
-    urlParams: {
-      apiQueryName: "GenByFuel",
-      searchParams: {
-        fuel: "nuclear",
-        from: "2020-10-05",
-        to: "2020-10-06",
-      },
-      // searchParams: { fuel: "nuclear", startTime: "2020-10-05" },
-    },
-    chartParams: powerChartParams,
-  };
   return (
     <div>
       <h1>French power generation</h1>
       <h2>Nuclear</h2>
-      <DemoChart {...nuclearProps} />;
-      {/* <DemoChart apiQueryName="GenByFuel" fuel="fossil_gas" />;
-      <DemoChart apiQueryName="GenByFuel" fuel="fossil_hard_coal" />;
-      <DemoChart apiQueryName="GenByFuel" fuel="hydro_water_reservoir" />;
-      <DemoChart
-        apiQueryName="GenByFuel"
-        fuel="hydro_run_of_river_and_poundage"
-      />
-      ;
-      <DemoChart apiQueryName="GenByFuel" fuel="hydro_pumped_storage" />;
-      <DemoChart apiQueryName="GenByFuel" fuel="wind_onshore" />;
-      <DemoChart apiQueryName="GenByFuel" fuel="solar" />;
-      <DemoChart apiQueryName="GenByFuel" fuel="fossil_oil" />;
-      <DemoChart apiQueryName="GenByFuel" fuel="biomass" />;
-      <DemoChart apiQueryName="GenByFuel" fuel="waste" />; */}
+      <DemoChart {...getProps("nuclear")} />;<h2>Gas</h2>
+      <DemoChart {...getProps("fossil_gas")} />;<h2>Coal</h2>
+      <DemoChart {...getProps("fossil_hard_coal")} />;<h2>Hydro Reservoir</h2>
+      <DemoChart {...getProps("hydro_water_reservoir")} />;
+      <h2>Hydro Run-of-river</h2>
+      <DemoChart {...getProps("hydro_run_of_river_and_poundage")} />;
+      <h2>Hydro Pumped Storage</h2>
+      <DemoChart {...getProps("hydro_pumped_storage")} />;<h2>Wind</h2>
+      <DemoChart {...getProps("wind_onshore")} />;<h2>Solar</h2>
+      <DemoChart {...getProps("solar")} />;<h2>Oil</h2>
+      <DemoChart {...getProps("fossil_oil")} />;<h2>Biomass</h2>
+      <DemoChart {...getProps("biomass")} />;<h2>Waste</h2>
+      <DemoChart {...getProps("waste")} />;
     </div>
   );
 }
 
+function getProps(fuel) {
+  const powerChartParams = { x: "start_time", y: "mw_value" };
+  const fromDate = "2020-10-06";
+  const apiQueryName = "GenByFuel";
+
+  return {
+    urlParams: {
+      apiQueryName: apiQueryName,
+      searchParams: {
+        fuel: fuel,
+        from: fromDate,
+      },
+    },
+    chartParams: powerChartParams,
+  };
+}
 export default App;
