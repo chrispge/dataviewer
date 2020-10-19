784c12e0b0e52f04ebd919a3d83699a0eaf79d84
Chris Page <cjpage@me.com>
2020-10-19 10:19:20 +0100

    use className instead of class

--- src/App.js
+++ src/App.js
@@ -5,43 +5,51 @@ import RTEGenByFuel from "./components/rte-gen-by-fuel";
 import RTEGenByUnit from "./components/rte-gen-by-unit";
 
 function App() {
-  const [activeCpt, setActiveCpt] = useState("RTEGenByUnit");
+  const [activeCpt, setActiveCpt] = useState("Spreads");
   console.log(activeCpt);
   return (
     <div>
-      <div class="wrapper">
-        <div class="header">Data viewer thing</div>
-        <div class="body"></div>
-        <div class="box sidebar">
-          <button class="sidebar-btn" onClick={() => setActiveCpt("Prices")}>
+      <div className="wrapper">
+        <div className="header">Data viewer thing</div>
+        <div className="body"></div>
+        <div className="box sidebar">
+          <button
+            className="sidebar-btn"
+            onClick={() => setActiveCpt("Prices")}
+          >
             Prices
           </button>
-          <button class="sidebar-btn" onClick={() => setActiveCpt("Spreads")}>
+          <button
+            className="sidebar-btn"
+            onClick={() => setActiveCpt("Spreads")}
+          >
             Spreads
           </button>
           <button
-            class="sidebar-btn"
+            className="sidebar-btn"
             onClick={() => setActiveCpt("RTEGenByFuel")}
           >
             French Generation By Fuel
           </button>
           <button
-            class="sidebar-btn"
+            className="sidebar-btn"
             onClick={() => setActiveCpt("RTEGenByUnit")}
           >
             French Generation By Unit
           </button>
         </div>
-        <div class="box main-content chart-container">
+        <div className="box main-content chart-container">
           {activeCpt === "Prices" && Prices()}
           {activeCpt === "Spreads" && Spreads()}
           {activeCpt === "RTEGenByFuel" && RTEGenByFuel()}
           {activeCpt === "RTEGenByUnit" && RTEGenByUnit({ fuel: "nuclear" })}
         </div>
-        <div class="box sidebar2">
+        <div className="box sidebar2">
           Sidebar2: I haven't decided what to put here yet
         </div>
-        <div class="box footer">© Chris Page {new Date().getFullYear()}</div>
+        <div className="box footer">
+          © Chris Page {new Date().getFullYear()}
+        </div>
       </div>
     </div>
   );
