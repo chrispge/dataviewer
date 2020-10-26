484fd1162d456d40160061dbbe461cb779fe2d07
Chris Page <cjpage@me.com>
2020-10-26 14:00:55 +0000

    remove Loading... when load unit names

--- src/components/rte-gen-by-unit.js
+++ src/components/rte-gen-by-unit.js
@@ -29,7 +29,7 @@ function RTEGenByUnit(props) {
     }));
     return chartInputs.map((inputs) => renderGenByUnit(inputs));
   } else {
-    return <div>Loading...</div>;
+    return null;
   }
 }
 
