44d421802383c0aa1e9509f917c283c97298e9be
Chris Page <cjpage@me.com>
2020-11-17 07:45:58 +0000

    separate header cpt

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default function () {
+  return (
+    <Typography variant="h1" align="center">
+      Data Viewer
+    </Typography>
+  );
+}
